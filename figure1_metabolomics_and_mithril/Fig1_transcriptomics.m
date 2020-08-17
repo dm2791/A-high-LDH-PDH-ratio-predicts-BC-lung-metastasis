e9d16d26a4f82dd5e9328e9dbce619a424e5cea7
Joao Xavier <joaobxavier@gmail.com>
2020-08-17 11:58:48 +0100

    Moved the rest of the Fig 1 and supp fig 1 code to its own directory
    Also did minor changes to the code so it ran on my mac.

--- figure1_metabolomics_and_mithril/Fig1_transcriptomics.m
+++ figure1_metabolomics_and_mithril/Fig1_transcriptomics.m
@@ -7,18 +7,18 @@ annotation=chipfile(26:end,1:19);
 
 %annotate lung IDs to gene IDs
 lunggsea=readtable('RNA_lungGSEA2.xlsx');
-getIDslung=intersect(lunggsea.x_1_2,annotation.x__ForInformationAboutTheAnnotationFileContent);
+getIDslung=intersect(lunggsea{:, 1},annotation.x__ForInformationAboutTheAnnotationFileContent);
 rowsl=ismember(annotation.x__ForInformationAboutTheAnnotationFileContent,getIDslung);
 geneIDlung=annotation.Var15(rowsl);
 
 braingsea=readtable('RNA_brainGSEA2.xlsx');
-getIDsbrain=intersect(braingsea.x_1_2,annotation.x__ForInformationAboutTheAnnotationFileContent);
+getIDsbrain=intersect(braingsea{:, 1},annotation.x__ForInformationAboutTheAnnotationFileContent);
 rowsb=ismember(annotation.x__ForInformationAboutTheAnnotationFileContent,getIDsbrain);
 geneIDbrain=annotation.Var15(rowsb);
 
 %% heatmaps
 %get hallmark glycolysis genes
-glyclistbig=readtable('geneset(1).txt');
+glyclistbig=readtable('geneset.txt');
 glyclist=table2array(glyclistbig);
 glycbrain=[];
 glyclung=[];
@@ -27,13 +27,13 @@ for i=1:length(glyclist)
     indl=find(strcmp(geneIDlung,glyclist(i)));
     if isempty(indb)
     elseif length(indb)>1
-    glycbrain=[glycbrain;indb(2)];
+        glycbrain=[glycbrain;indb(2)];
     else
         glycbrain=[glycbrain;indb];
     end
     if isempty(indl)
     elseif length(indl)>1
-    glyclung=[glyclung;indl(2)];
+        glyclung=[glyclung;indl(2)];
     else
         glyclung=[glyclung;indl];
     end
@@ -43,13 +43,18 @@ glycbrain=glycbrain+2; %align properly
 glyclung=glyclung+2;
 
 glycbraindata=table2array(braingsea(glycbrain,4:end));
-glycbraindata=cellfun(@str2num,glycbraindata);
+if ~isnumeric(glycbraindata)
+    glycbraindata=cellfun(@str2num,glycbraindata);
+end
+
 glyclungdata=table2array(lunggsea(glyclung,4:end));
-glyclungdata=cellfun(@str2num,glyclungdata);
+if ~isnumeric(glyclungdata)
+    glyclungdata=cellfun(@str2num,glyclungdata);
+end
 
 %FC and normalize
 brainfc=mean(glycbraindata(:,4:5),2)./mean(glycbraindata(:,1:3),2);
 lungfc=mean(glyclungdata(:,3:6),2)./mean(glyclungdata(:,1:2),2);
 datafc=[log2(brainfc),log2(lungfc)];
 clustergram(datafc,'Symmetric','false','Colormap','redbluecmap');
-%set(ans,'RowLabels',geneIDlung(glyclung-2))
+set(ans,'RowLabels',geneIDlung(glyclung-2))
