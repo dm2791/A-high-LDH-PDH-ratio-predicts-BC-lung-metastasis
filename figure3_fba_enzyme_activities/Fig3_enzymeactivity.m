3570b26c31afcb8bd87fa0e7736fc0f04a7b067c
dm2791 <69275400+dm2791@users.noreply.github.com>
2020-08-31 10:47:36 -0400

    Update Fig3_enzymeactivity.m

--- figure3_fba_enzyme_activities/Fig3_enzymeactivity.m
+++ figure3_fba_enzyme_activities/Fig3_enzymeactivity.m
@@ -1,6 +1,6 @@
 %% analyze hexokinase data
 
-hexokinase_50min=xlsread('hexokinase50min.xlsx','A38:BA43');
+hexokinase_50min=readmatrix('hexokinasedatamat.csv');
 standard=[];
 j=1;
 for i=3:8
@@ -112,7 +112,7 @@ pvalL=mdl.Coefficients(3,6).pValue;
 
 %% analyze pfk data
 
-pfk_5min=xlsread('PFK5min.xlsx','A38:BA43');
+pfk_5min=readmatrix('pfkdatamat.csv');
 standard=[];
 j=1;
 for i=3:8
@@ -223,7 +223,7 @@ pvalL=mdl.Coefficients(3,6).pValue;
 
 %% analyze g3pdh data
 
-g3pdh_0min=xlsread('G3PDHREPEAT0MIN.xlsx','A38:BA43');
+g3pdh_0min=readmatrix('g3pdhdatamat.csv');
 standard=[];
 j=1;
 for i=3:8
@@ -335,7 +335,7 @@ pvalL=mdl.Coefficients(3,6).pValue;
 
 %% analyze pk data
 
-pk_fl0min=xlsread('PK0min.xlsx','A38:BA43');
+pk_fl0min=readmatrix('pkdatamat.csv');
 standard=[];
 j=1;
 for i=3:8
