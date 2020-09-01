24119c93c52386398faeb0249a00e22e8d958191
dm2791 <69275400+dm2791@users.noreply.github.com>
2020-08-31 22:03:49 -0400

    Update Fig1Mithril.m

--- figure1_metabolomics_and_mithril/Fig1Mithril.m
+++ figure1_metabolomics_and_mithril/Fig1Mithril.m
@@ -1,6 +1,6 @@
 %% analyze mithril output
 %% waterfall plots
-lungout=readtable('output_mithril_lung.txt');
+lungout=readtable('lungoutput_mithril.csv');
 ranked_lungout=sortrows(lungout,3);
 figure
 b=bar(ranked_lungout.RawAccumulator);
@@ -14,7 +14,7 @@ b.CData(nonsig,:) = repmat([.7 .7 .7],length(nonsig),1);
 glyc=find(contains(ranked_lungout.PathwayName,'Glycolysis'));
 b.CData(glyc,:) = [0 0 0];
 
-brainout=readtable('mithriloutputbrainh.xls');
+brainout=readtable('brainoutput_mithril.csv');
 ranked_lungout=sortrows(lungout,3);
 figure
 b=bar(ranked_lungout.RawAccumulator);
@@ -32,7 +32,7 @@ set(cgo,'RowLabels',pathway_both(1:61))
 labelnew=flip(cgo.RowLabels); %so I can rotate fig in illustrator
 
 %% heatmap brain vs lung
-bvslout=readtable('mithriloutputbrm2lm2.txt');
+bvslout=readtable('brm2lm2output_mithril.csv');
 pathway_bl=bvslout.PathwayName;
 heatmaptable_bl=table(pathway_bl,bvslout.CorrectedAccumulator,zeros(249,1),bvslout.AdjustedPValue);
 cgo=clustergram(table2array(heatmaptable_bl(1:61,2:3)),'Colormap','redbluecmap','Standardize','column');
